93be7e6532a2a956ac40021ab137050528d9e29d
João Jerónimo <joao.jeronimo.pro@gmail.com>
2019-10-27 16:01:27 +0000

    Scripts e configs do cleanfeed.

--- 2nd_phase.bash
+++ 2nd_phase.bash
@@ -1,2 +1,17 @@
-git clone https://github.com/crooks/cleanfeed.git Cleanfeed-git
 
+export BACKDIR=`mktemp --tmpdir="$HOME" -d`
+
+
+#git clone https://github.com/crooks/cleanfeed.git Cleanfeed-git
+
+mkdir /opt/inn2/etc/cleanfeed/
+cp Cleanfeed-git/samples/* /opt/inn2/etc/cleanfeed/
+cp /opt/inn2/bin/filter/filter_innd.pl "$BACKDIR"/filter_innd.pl
+cp Cleanfeed-git/cleanfeed /opt/inn2/bin/filter/filter_innd.pl
+
+# Verificação de sintaxe:
+perl -wc /opt/inn2/bin/filter/filter_innd.pl
+perl -wc /opt/inn2/etc/cleanfeed/cleanfeed.local
+
+echo "Caso não haja erros, executar:"
+echo "ctlinnd reload filter.perl meow"
